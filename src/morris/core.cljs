9f6e6261a49cb5592a2e10b623960e033b5c9a7e
Nikita Antonenkov <krigar1184@gmail.com>
2025-07-29 18:31:43 +0300

    up

--- src/morris/core.cljs
+++ src/morris/core.cljs
@@ -1,27 +1,87 @@
 (ns morris.core
-  (:require [reagent.dom.client :as rdomc]))
+  (:require [reagent.core :as r]
+            [reagent.dom.client :as rdomc]
+            [cljs.core :as c]))
 
 (set! *warn-on-infer* true)
 
 (defonce app-container (.getElementById js/document "app"))
 (defonce root (delay (rdomc/create-root app-container)))
 
-(def init-coords {'(0 0) false '(0 3) false '(0 6) false
-                  '(1 1) false '(1 3) false '(1 5) false
-                  '(2 2) false '(2 3) false '(2 4) false
-                  '(3 0) false '(3 1) false '(3 2) false  '(3 4) false '(3 5) false '(3 6) false
-                  '(4 2) false '(4 3) false '(4 4) false
-                  '(5 1) false '(5 3) false '(5 5) false
-                  '(6 0) false '(6 3) false '(6 6) false})
+(def init-coords {'(0 0) nil '(0 3) nil '(0 6) nil
+                  '(1 1) nil '(1 3) nil '(1 5) nil
+                  '(2 2) nil '(2 3) nil '(2 4) nil
+                  '(3 0) nil '(3 1) nil '(3 2) nil  '(3 4) nil '(3 5) nil '(3 6) nil
+                  '(4 2) nil '(4 3) nil '(4 4) nil
+                  '(5 1) nil '(5 3) nil '(5 5) nil
+                  '(6 0) nil '(6 3) nil '(6 6) nil})
 
-(defn board []
+(def current-coords (r/atom init-coords))
+
+; state
+(def game-status (r/atom :not-started))
+
+(def player1 {:name (r/atom "player 1") :men-left (r/atom 9) :color :white})
+(def player2 {:name (r/atom "player 2") :men-left (r/atom 9) :color :black})
+
+(def turn (r/atom 0))
+
+(defn- on-square-click [i j]
+  (let* [key (list i j)
+         new-value (get @current-coords key)]
+        (do (swap! current-coords assoc key (not new-value))
+            (swap! turn inc))))
+
+(defn- ui-board []
+  (let* [coords @current-coords
+         player (if (odd? @turn) player1 player2)]
+        [:div [:div.header
+               [:p (str @(get player :name) " turn")]
+               [:p (get {} :name "")]]
+         [:div.board
+          (for [i (range 0 7) j (range 0 7)]
+            (let* [k (list i j)]
+                  (if (contains? coords k)
+                    ^{:key (str i j)} [:div.square
+                                       {:on-click #(apply on-square-click [i j])}
+                                       (if (nil? (get coords k)) \u25cf
+                                           [:span {:style {:color (get coords k)}} \u25c9])]
+                    ^{:key (str i j)} [:div.square.empty])))]]))
+
+(defn- init-game []
+  (do
+    (reset! game-status :started)
+    (.log js/console "game started")))
+
+(defn- reset-game [_]
+  (reset! current-coords init-coords)
+  (reset! turn 0)
+  (reset! game-status :placing))
+
+(defn- set-player-name [event] ; TODO refactor
+  (if (= (-> event .-target .-name) "player1_name") (reset! (get player1 :name) (-> event .-target .-value))
+      (reset! (get player2 :name) (-> event .-target .-value))))
+
+(defn- ui-start []
   [:div.container
-   [:div.board
-    (for [i (range 0 7) j (range 0 7)]
-      (if (contains? init-coords (list i j))
-        ^{:key (str i j)} [:div.square {:style {:text-align :left}}
-                           [:span i j]]
-        ^{:key (str i j)} [:div.square.empty]))]])
+   (cond (= @game-status :not-started)
+         [:div.start
+          [:input {:type :button
+                   :value "Start"
+                   :on-click #(init-game)}]]
+         (= @game-status :started)
+         [:div
+          [:label {:id "player1_name"}]
+          [:input {:name "player1_name" :default-value @(get player1 :name) :type :text :on-blur set-player-name}]
+          [:br] [:br]
+          [:label {:id "player2_name"}]
+          [:input {:name "player2_name" :default-value @(get player2 :name) :type :text :on-blur set-player-name}]
+          [:br] [:br] [:input {:type :button :value "Play!" :on-click #(do (swap! turn inc) (reset! game-status :placing))}]]
+         (= @game-status :placing) [:div [ui-board]
+                                    [:br]
+                                    [:div.footer
+                                     [:input {:type :button :value "Reset game" :on-click reset-game}]]])])
 
 (defn ^:export ^:dev/after-load run []
-  (rdomc/render @root [board]))
+  (rdomc/render @root [ui-start]))
+
